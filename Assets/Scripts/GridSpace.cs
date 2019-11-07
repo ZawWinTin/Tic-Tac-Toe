c528df65033a7a3a9a25516a40ae60c17bd8ecd8
Zaw Win Tin <www.shadowbeast@gmail.com>
2019-11-08 01:05:40 +0630

    first commit

---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour {
+
+    public Button button;
+    public Text buttonText;
+    private GameController gameController;
+    public void SetSpace(int id)
+    {
+        if((!gameController.isBot) || (gameController.currentSide == gameController.playerSide))
+        {
+            buttonText.text = gameController.GetCurrentSide();
+            button.interactable = false;
+            button.image.sprite =gameController.clickedbuttonicon;
+            gameController.AddId(id);
+            gameController.EndTurn();
+        }  
+    }
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+}
